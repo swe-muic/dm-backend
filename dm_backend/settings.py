1d9467672dcbc3ad6e33075a4e00208a90d296c5
DrNattapoom <nattapoom.dr@gmail.com>
2023-03-06 08:11:26 +0700

    added .gitignore

--- dm_backend/settings.py
+++ dm_backend/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = "dm_backend.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
 
