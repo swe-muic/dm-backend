93dea520b8a17648ca73fc1fe08c099471b941b0
DrNattapoom <nattapoom.dr@gmail.com>
2023-03-06 22:40:31 +0700

    added sign-up api

--- dm_backend/serializers/user.py
+++ dm_backend/serializers/user.py
@@ -12,15 +12,9 @@ class UserSerializer(serializers.ModelSerializer):
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        """
-        Create and return a new `User` instance, given the validated data.
-        """
         return User.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
-        """
-        Update and return an existing `User` instance, given the validated data.
-        """
         instance.username = validated_data.get('username', instance.username)
         instance.password = validated_data.get('password', instance.password)
         instance.save()
