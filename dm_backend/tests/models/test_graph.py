cd9369abe3bfd9533c274e6441b8f1b19aaf2781
suhavni <suhavni@gmail.com>
2023-03-13 21:12:01 +0700

    added graph model test cases

---
+++ dm_backend/tests/models/test_graph.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+
+from dm_backend.tests.baker_recipes.graph_baker_recipe import graph_recipe
+from dm_backend.tests.baker_recipes.user_baker_recipe import user_recipe
+
+
+class TestGraph(TestCase):
+    def test_create_graph(self):
+        graph_name = "graph1"
+        graph = graph_recipe.make(name=graph_name)
+        self.assertEqual(graph.name, graph_name)
+
+    def test_create_graph_with_owner(self):
+        owner_name = "owner1"
+        owner = user_recipe.make(username=owner_name)
+        graph = graph_recipe.make(owner=owner)
+        self.assertEqual(graph.owner.username, owner_name)
