ecb2883d4953596667b4c62f26ca17d2492c561e
DrNattapoom <nattapoom.dr@gmail.com>
2023-03-22 14:27:20 +0700

    remove unnecessary code

--- dm_backend/src/serializers/equation.py
+++ dm_backend/src/serializers/equation.py
@@ -32,36 +32,3 @@ class EquationSerializer(serializers.ModelSerializer):
             "line_width",
             "graph",
         ]
-
-    def create(self, validated_data: dict) -> Equation:
-        """
-        Create and return a new Equation instance using the validated data.
-
-        Args:
-            validated_data (dict):  The validated data for the Equation instance.
-
-        Returns:
-            Equation: A new Equation instance created using the validated data.
-        """
-        return Equation.objects.create(**validated_data)
-
-    def update(self, instance: Equation, validated_data: dict) -> Equation:
-        """
-        Update and return an existing Equation instance, given the validated data.
-
-        Args:
-            instance (Equation): The Equation instance to update.
-            validated_data (dict): The validated data to update the Equation instance with.
-
-        Returns:
-            Equation: The updated Equation instance.
-        """
-        instance.equation = validated_data.get("equation", instance.equation)
-        instance.parsed_equation = validated_data.get(
-            "parsed_equation", instance.parsed_equation
-        )
-        instance.color = validated_data.get("color", instance.color)
-        instance.line_style = validated_data.get("line_style", instance.line_style)
-        instance.line_width = validated_data.get("line_width", instance.line_width)
-        instance.save()
-        return instance
