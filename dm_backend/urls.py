32b2144cbd88c506e949c20f520c0f5c0a369dd3
DrNattapoom <nattapoom.dr@gmail.com>
2023-04-09 15:38:11 +0700

    refactor equation api to use viewsets

--- dm_backend/urls.py
+++ dm_backend/urls.py
@@ -14,20 +14,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
 
-from .src.views.equation import EquationAPI
+from .src.views.equation import EquationAPIViewSet
 from .src.views.equation_parser import EquationParserAPI
 from .src.views.graph import GraphAPI
 
+router = routers.DefaultRouter()
+router.register(r"api/viewset/equations", EquationAPIViewSet)
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include(router.urls)),
     path("api/equations/parser/", EquationParserAPI.as_view(http_method_names=["get"])),
-    path("api/equations/", EquationAPI.as_view(http_method_names=["get", "post"])),
-    path(
-        "api/equations/<int:equation_id>/",
-        EquationAPI.as_view(http_method_names=["get", "put", "delete"]),
-    ),
     path("api/graphs/", GraphAPI.as_view(http_method_names=["get", "post"])),
     path(
         "api/graphs/<int:graph_id>/",
