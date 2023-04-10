70c47312867048937955d0d64644339edd26a469
DrNattapoom <nattapoom.dr@gmail.com>
2023-04-10 23:59:08 +0700

    add graph session support to parser

--- dm_backend/src/views/equation_parser.py
+++ dm_backend/src/views/equation_parser.py
@@ -2,11 +2,12 @@
 EquationParserAPI is a view that simplifies a LaTeX equation and returns the result as a JSON response with http status.
 
 The view accepts GET requests with equation data in the query parameters.
-- With GET request, the equation data is parsed using the simplify_latex_expression method of the Expression class,
+- With POST request, the equation data is parsed using the simplify_latex_expression method of the Expression class,
   from latexvm, and if successful, a new data object is returned in the response.
 """
 
-from latexvm.expression import Expression
+from latexvm.graph_session import GraphSession
+from latexvm.type_defs import ResultType
 from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.request import Request
@@ -23,22 +24,36 @@ class EquationParserAPIViewSet(ViewSet):
     renderer_classes = [APIRenderer]
 
     @action(detail=False, methods=["post"])
-    def parse_equation(self, request: Request) -> Response:
+    def parse_expressions(self, request: Request) -> Response:
         """
-        Parse and simplify the LaTeX expression of an equation.
+        Resolve and parse a given list of a mathematical expressions containing a function call.
+
+        The function call will be resolved by substituting it with its value based on the function defined in the
+        environment. The resulting expression will then be parsed as a LaTeX string.
 
         Args:
             request (Request): The HTTP request object.
 
         Returns:
-            Response: A JSON response containing the original and parsed LaTeX expression,
+            Response: A JSON response containing the original and parsed expressions,
                 or an error response if there was a problem with the input or server.
         """
         try:
-            equation = request.data["equation"]
+            expressions = request.data["expressions"]
+            graph_session = GraphSession.new()
+            graph_session.add_sub_rule(r"\*\*", "^")
+            parsed_expressions = []
+            for expression in expressions:
+                simplified_expression = graph_session.execute(expression).message
+                parsed_expression = graph_session.force_resolve_function(
+                    simplified_expression
+                )
+                if parsed_expression.result == ResultType.FAILURE:
+                    raise LaTeXParsingError(f"{expression} is invalid")
+                parsed_expressions.append(parsed_expression.message)
             data = {
-                "equation": equation,
-                "parsed_equation": Expression.simplify_latex_expression(equation),
+                "expressions": expressions,
+                "parsed_expressions": parsed_expressions,
             }
             return Response(data, status=status.HTTP_200_OK)
         except LaTeXParsingError as e:
