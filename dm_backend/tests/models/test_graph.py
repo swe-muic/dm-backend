73606b1969886dc7f8ba1b4997c81d3e85577d1f
DrNattapoom <nattapoom.dr@gmail.com>
2023-04-11 14:45:17 +0700

    update database schema for graph model

--- dm_backend/tests/models/test_graph.py
+++ dm_backend/tests/models/test_graph.py
@@ -1,7 +1,6 @@
 from django.test import TestCase
 
 from dm_backend.tests.baker_recipes.graph_baker_recipe import graph_recipe
-from dm_backend.tests.baker_recipes.user_baker_recipe import user_recipe
 
 
 class TestGraph(TestCase):
@@ -11,7 +10,11 @@ class TestGraph(TestCase):
         self.assertEqual(graph.name, graph_name)
 
     def test_create_graph_with_owner(self):
-        owner_name = "owner1"
-        owner = user_recipe.make(username=owner_name)
-        graph = graph_recipe.make(owner=owner)
-        self.assertEqual(graph.owner.username, owner_name)
+        owner_uid = "firebase_uid"
+        graph = graph_recipe.make(owner=owner_uid)
+        self.assertEqual(graph.owner, owner_uid)
+
+    def test_create_graph_with_preview(self):
+        preview = "preview_minio_bucket_name"
+        graph = graph_recipe.make(preview=preview)
+        self.assertEqual(graph.preview, preview)
--- dm_backend/tests/serializers/test_graph.py
+++ dm_backend/tests/serializers/test_graph.py
@@ -6,32 +6,14 @@ from dm_backend.src.serializers.graph import GraphSerializer
 
 class GraphSerializerTest(TestCase):
     def setUp(self):
+        self.create_url = "/api/graphs/"
+        self.update_url = lambda graph_id: f"/api/graphs/{graph_id}/"
         self.validated_data = {
             "name": "test_graph",
             "owner": "test_owner",
         }
-        self.graph = Graph.objects.create(**self.validated_data)
-        self.serializer = GraphSerializer(instance=self.graph)
-
-    def test_contains_expected_fields(self):
-        """Test that the serializer contains the expected fields."""
-        data = self.serializer.data
-        self.assertCountEqual(
-            data.keys(), ["id", "name", "preview", "owner", "created", "updated"]
-        )
-
-    def test_equation_field_content(self):
-        """Test that the name field has the expected content."""
-        data = self.serializer.data
-        self.assertEqual(data["name"], self.validated_data["name"])
-
-    def test_parsed_equation_field_content(self):
-        """Test that the owner field has the expected content."""
-        data = self.serializer.data
-        self.assertEqual(data["owner"], self.validated_data["owner"])
 
     def test_create_graph(self):
-        """Test that the graph instance is created correctly."""
         serializer = GraphSerializer()
         graph = serializer.create(self.validated_data)
         self.assertIsInstance(graph, Graph)
@@ -39,7 +21,6 @@ class GraphSerializerTest(TestCase):
         self.assertEqual(graph.owner, self.validated_data["owner"])
 
     def test_update_graph(self):
-        """Test that the graph instance is updated correctly."""
         graph = Graph.objects.create(**self.validated_data)
         serializer = GraphSerializer()
         updated_graph = serializer.update(
