cf5ff96c5d0e434b7c2b84b2529566b101c1e41f
DrNattapoom <nattapoom.dr@gmail.com>
2023-04-11 14:45:17 +0700

    update database schema for graph model

--- dm_backend/src/models/graph.py
+++ dm_backend/src/models/graph.py
@@ -1,6 +1,5 @@
 """Graph model module."""
 
-from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -9,13 +8,15 @@ class Graph(models.Model):
 
     Attributes:
         name (str): The name of the graph.
-        owner (User): The owner of the graph.
+        preview (str): The name of the minio bucket that stores graph preview.
+        owner (str): The firebase uid of the owner of the graph.
         created (datetime): The date and time the graph was created.
         updated (datetime): The date and time the graph was last updated.
     """
 
     name = models.CharField(max_length=100)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.CharField(max_length=255)
+    preview = models.CharField(max_length=255, null=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
